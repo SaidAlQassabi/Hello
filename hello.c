e08ab8fe7739c3a30f3068f9b7580368775efbae
Said Al-Qassabi <said2011salim@hotmail.com>
2019-01-17 13:02:21 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,3 +1,14 @@
-int main(int argc, char **argv)
+/**
+ * Author: Said Al-Qassabi
+ * Date: 2019/01/017
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
 
-printf("Said Al-Qassabi - Software Engineering");
+  printf("Said Al-Qassabi - Software Engineering\n");
+
+  return 0;
+}
